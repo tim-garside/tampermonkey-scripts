163de14abc215ac1ae038b615cbde99cc23ec382
tim-garside <86387199+tim-garside@users.noreply.github.com>
2025-06-17 14:57:32 +1000

    Update Copilot Message Exporter - RCP.user.js

--- Copilot Message Exporter - RCP.user.js
+++ Copilot Message Exporter - RCP.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Copilot Message Exporter - Rapid Course Prototype
+// @name         Copilot Message Exporter - RCP
 // @namespace    http://tampermonkey.net/
 // @version      2025-06-17
 // @description  try to take over the world!
